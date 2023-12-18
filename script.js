3abfd6dcc6ce3f941e70f34916fd5c68d250552f
MeghanaBait <133511757+MeghanaBait@users.noreply.github.com>
2023-12-18 12:39:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function fibonacci(num) {
 // your code here
+	if(num == 1 || num == 2){
+		return num - 1;
+	}else{
+		let i = 2;
+		let first = 0;
+		let second = 1;
+		let ans =0;
+		while(i < num){
+			ans = first + second;
+			first = second;
+			second = ans;
+			i++;
+		}
+		console.log(ans);
+		return ans;
+	}
+
 }
 
 module.exports = fibonacci;
